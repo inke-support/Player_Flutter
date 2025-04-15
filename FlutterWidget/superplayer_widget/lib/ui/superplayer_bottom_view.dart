0b74d5b0b60f9ae318859d1d9ad6dd4499493d82
ibesty <11418619+ibesty@users.noreply.github.com>
2025-04-15 17:39:14 +0800

    feat: 直播状态下不显示进度条

--- FlutterWidget/superplayer_widget/lib/ui/superplayer_bottom_view.dart
+++ FlutterWidget/superplayer_widget/lib/ui/superplayer_bottom_view.dart
@@ -6,7 +6,8 @@ class VideoBottomView extends StatefulWidget {
   final SuperPlayerController _playerController;
   final BottomViewController _controller;
 
-  const VideoBottomView(this._playerController, this._controller, Key key) : super(key: key);
+  const VideoBottomView(this._playerController, this._controller, Key key)
+      : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -43,8 +44,10 @@ class _VideoBottomViewState extends State<VideoBottomView> {
       _bufferedDuration = 0;
     }
 
-    _isPlayMode = (widget._playerController.playerState == SuperPlayerState.PLAYING);
-    bool isFullScreen = widget._playerController._playerUIStatus == SuperPlayerUIStatus.FULLSCREEN_MODE;
+    _isPlayMode =
+        (widget._playerController.playerState == SuperPlayerState.PLAYING);
+    bool isFullScreen = widget._playerController._playerUIStatus ==
+        SuperPlayerUIStatus.FULLSCREEN_MODE;
     _showFullScreenBtn = !isFullScreen;
     _isShowQuality = isFullScreen;
     _currentQuality = widget._playerController.currentQuality;
@@ -70,11 +73,14 @@ class _VideoBottomViewState extends State<VideoBottomView> {
                     ? Container(
                         decoration: const BoxDecoration(
                             color: Color(ColorResource.COLOR_TRANS_BLACK),
-                            borderRadius: BorderRadius.all(Radius.circular(50))),
-                        padding: const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50))),
+                        padding: const EdgeInsets.only(
+                            left: 10, right: 10, top: 5, bottom: 5),
                         child: Text(
                           "${Utils.formattedTime(showedKeyFrameInfo!.time)} ${showedKeyFrameInfo!.content}",
-                          style: const TextStyle(color: Colors.white, fontSize: 14),
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 14),
                           softWrap: true,
                         ),
                       )
@@ -83,7 +89,8 @@ class _VideoBottomViewState extends State<VideoBottomView> {
           Container(
             decoration: const BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage("images/superplayer_bottom_shadow.png", package: PlayerConstants.PKG_NAME),
+                    image: AssetImage("images/superplayer_bottom_shadow.png",
+                        package: PlayerConstants.PKG_NAME),
                     fit: BoxFit.fill)),
             padding: _showFullScreenBtn
                 ? const EdgeInsets.only(left: 6, right: 6, bottom: 3)
@@ -121,7 +128,8 @@ class _VideoBottomViewState extends State<VideoBottomView> {
             padding: const EdgeInsets.only(left: 5, right: 10),
             child: _currentQuality != null
                 ? Text(
-                    VideoQualityUtils.transformToQualityName(_currentQuality!.title),
+                    VideoQualityUtils.transformToQualityName(
+                        _currentQuality!.title),
                     style: const TextStyle(fontSize: 12, color: Colors.white),
                   )
                 : Container(),
@@ -136,12 +144,14 @@ class _VideoBottomViewState extends State<VideoBottomView> {
           ? const Image(
               width: 30,
               height: 30,
-              image: AssetImage("images/superplayer_ic_vod_pause_normal.png", package: PlayerConstants.PKG_NAME),
+              image: AssetImage("images/superplayer_ic_vod_pause_normal.png",
+                  package: PlayerConstants.PKG_NAME),
             )
           : const Image(
               width: 30,
               height: 30,
-              image: AssetImage("images/superplayer_ic_vod_play_normal.png", package: PlayerConstants.PKG_NAME),
+              image: AssetImage("images/superplayer_ic_vod_play_normal.png",
+                  package: PlayerConstants.PKG_NAME),
             ),
     );
   }
@@ -154,51 +164,57 @@ class _VideoBottomViewState extends State<VideoBottomView> {
         child: const Image(
           width: 30,
           height: 30,
-          image: AssetImage("images/superplayer_ic_vod_fullscreen.png", package: PlayerConstants.PKG_NAME),
+          image: AssetImage("images/superplayer_ic_vod_fullscreen.png",
+              package: PlayerConstants.PKG_NAME),
         ),
       ),
     );
   }
 
   Widget _getSlider() {
-    return Expanded(
-      child: VideoSlider(
-        key: _sliderView,
-        min: 0,
-        max: _videoDuration,
-        value: _currentDuration,
-        bufferedValue: _bufferedDuration,
-        activeColor: const Color(ColorResource.COLOR_SLIDER_MAIN_THEME),
-        inactiveColor: const Color(ColorResource.COLOR_GRAY),
-        sliderColor: const Color(ColorResource.COLOR_SLIDER_MAIN_THEME),
-        sliderOutterColor: Colors.white,
-        progressHeight: 2,
-        sliderRadius: 4,
-        sliderOutterRadius: 10,
-        onPointClick: (double pointX, int pos) {
-          if (null != keyFrameList) {
-            setState(() {
-              showedKeyFrameInfo = keyFrameList![pos];
-            });
-          }
-        },
-        // Dragging the progress bar is prohibited during live streaming.
-        canDrag: _playerType == SuperPlayerType.VOD,
-        playPoints: !_showFullScreenBtn ? _playPoints : [],
-        onDragUpdate: (value) {
-          _isOnDraging = true;
-          _currentDuration = value * _videoDuration;
-          widget._controller.onSeekChanged(_currentDuration);
-        },
-        onDragEnd: (value) {
-          setState(() {
-            _isOnDraging = false;
+    return Visibility(
+      visible: _playerType == SuperPlayerType.VOD,
+      maintainSize: true,
+      child: Expanded(
+        child: VideoSlider(
+          key: _sliderView,
+          min: 0,
+          max: _videoDuration,
+          value: _currentDuration,
+          bufferedValue: _bufferedDuration,
+          activeColor: const Color(ColorResource.COLOR_SLIDER_MAIN_THEME),
+          inactiveColor: const Color(ColorResource.COLOR_GRAY),
+          sliderColor: const Color(ColorResource.COLOR_SLIDER_MAIN_THEME),
+          sliderOutterColor: Colors.white,
+          progressHeight: 2,
+          sliderRadius: 4,
+          sliderOutterRadius: 10,
+          onPointClick: (double pointX, int pos) {
+            if (null != keyFrameList) {
+              setState(() {
+                showedKeyFrameInfo = keyFrameList![pos];
+              });
+            }
+          },
+          // Dragging the progress bar is prohibited during live streaming.
+          canDrag: _playerType == SuperPlayerType.VOD,
+          playPoints: !_showFullScreenBtn ? _playPoints : [],
+          onDragUpdate: (value) {
+            _isOnDraging = true;
             _currentDuration = value * _videoDuration;
-            widget._playerController.seek(_currentDuration);
-            LogUtils.d(TAG, "_currentDuration:$_currentDuration,_videoDuration:$_videoDuration");
-            widget._controller.onSeekEnd();
-          });
-        },
+            widget._controller.onSeekChanged(_currentDuration);
+          },
+          onDragEnd: (value) {
+            setState(() {
+              _isOnDraging = false;
+              _currentDuration = value * _videoDuration;
+              widget._playerController.seek(_currentDuration);
+              LogUtils.d(TAG,
+                  "_currentDuration:$_currentDuration,_videoDuration:$_videoDuration");
+              widget._controller.onSeekEnd();
+            });
+          },
+        ),
       ),
     );
   }
@@ -215,11 +231,14 @@ class _VideoBottomViewState extends State<VideoBottomView> {
     widget._controller.onTapQuality();
   }
 
-  void updateDuration(double duration, double videoDuration, double bufferedDration) {
+  void updateDuration(
+      double duration, double videoDuration, double bufferedDration) {
     if (_isOnDraging) {
       return;
     }
-    if (duration != _currentDuration || _videoDuration != videoDuration || _bufferedDuration != bufferedDration) {
+    if (duration != _currentDuration ||
+        _videoDuration != videoDuration ||
+        _bufferedDuration != bufferedDration) {
       if (duration <= videoDuration) {
         setState(() {
           _currentDuration = duration;
@@ -296,5 +315,6 @@ class BottomViewController {
   Function(double value) onSeekChanged;
   Function onSeekEnd;
 
-  BottomViewController(this.onTapStart, this.onTapFullScreen, this.onTapQuality, this.onSeekChanged, this.onSeekEnd);
+  BottomViewController(this.onTapStart, this.onTapFullScreen, this.onTapQuality,
+      this.onSeekChanged, this.onSeekEnd);
 }
