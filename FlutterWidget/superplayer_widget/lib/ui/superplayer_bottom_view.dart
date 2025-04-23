456e402a345d88118d5485d9fb57b5bcd1a15f0e
ibesty <11418619+ibesty@users.noreply.github.com>
2025-04-23 18:55:24 +0800

    feat: 移除不需要的元素

--- FlutterWidget/superplayer_widget/lib/ui/superplayer_bottom_view.dart
+++ FlutterWidget/superplayer_widget/lib/ui/superplayer_bottom_view.dart
@@ -104,7 +104,7 @@ class _VideoBottomViewState extends State<VideoBottomView> {
                     style: const TextStyle(fontSize: 12, color: Colors.white),
                   ),
                 ),
-                // _getSlider(),
+                _getSlider(),
                 Text(
                   _buildTextString(_videoDuration),
                   style: const TextStyle(fontSize: 12, color: Colors.white),
@@ -174,7 +174,7 @@ class _VideoBottomViewState extends State<VideoBottomView> {
   Widget _getSlider() {
     return Expanded(
       child: Visibility(
-        visible: _playerType == SuperPlayerType.VOD,
+        visible: false,
         maintainSize: true,
         maintainAnimation: true,
         maintainState: true,
